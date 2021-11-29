24fe1ccdc384acd6ebd88dcb89fe89358727d811
htorres <htorres572@gmail.com>
2021-11-29 02:54:18 -0800

    fixed loca storage

--- scheduler.js
+++ scheduler.js
@@ -1,3 +1,13 @@
 $(currentDay).text(moment().format('dddd, MMMM Do'))
 currentHour = moment().format("hh")
 
+$("button").click(function () {
+  let hour = $(this).siblings(".form-control").attr("id")
+  let note = $(this).siblings(".form-control").val()
+  localStorage.setItem(hour,note)
+  console.log(hour,note)
+})
+
+// for each()
+
+
